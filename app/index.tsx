7ccf6d2c27abc743a825e0f7fbc82ea5b4efbfe3
Neo Zheng Weng <neozhengweng@gmail.com>
2025-05-28 00:01:49 +0800

    setup tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-500">Welcome</Text>
     </View>
   );
 }
