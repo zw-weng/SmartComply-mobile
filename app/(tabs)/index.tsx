c0c1fd14a8437e09c935973a234aa3b261c4c0ea
Neo Zheng Weng <neozhengweng@gmail.com>
2025-06-08 01:04:10 +0800

    tailwind & navigation bar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-500">Welcome</Text>
+      <Link href="/audit">Audit</Link>
+      <Link href="/profile">Profile</Link>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import './global.css'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
